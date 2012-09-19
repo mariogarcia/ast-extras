540867f34186068c4c8e03f21a046fd4fecc3597
Mario Garcia <satriani@malmsteens-MacBook-Pro.local>
2012-09-19 21:06:25 +0200

    Added author to class

--- src/test/groovy/com/github/groovy/astextras/local/methods/Silly.groovy
+++ src/test/groovy/com/github/groovy/astextras/local/methods/Silly.groovy
@@ -2,6 +2,8 @@ package com.github.groovy.astextras.local.methods
 
 /**
  * All transformation added to this class add methods to the bytecode
+ * 
+ * @author marioggar
 **/
 @ToJson()
 class Silly{
